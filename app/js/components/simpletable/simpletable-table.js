01c95f5174d48d006a47c7edcb2813fda9b8804a
Roy Bonilla <rbonilla@alivebox.com>
2015-03-25 14:11:08 -0600

    add new resize changes and directive

--- app/js/components/simpletable/simpletable-table.js
+++ app/js/components/simpletable/simpletable-table.js
@@ -1,85 +1,94 @@
 'use strict';
 
 var SimpleTableDirective = function(scope, element, attrs, $log, $timeout){
-  this.scope = scope;
-  this.element = element;
-  this.attrs = attrs;
-  this.$log = $log;
-  this.$timeout = $timeout;
-  this.plugins = [];
+    this.scope = scope;
+    this.element = element;
+    this.attrs = attrs;
+    this.$log = $log;
+    this.$timeout = $timeout;
+    this.plugins = [];
 
-  this.init();
-  $log.log("SimpleTable created: ", this.scope);
+    this.init();
+    $log.log('SimpleTable created: ', this.scope);
 };
+
 SimpleTableDirective.prototype.init = function(){
-  this.addListeners();
-  this.validateConfig();
+      this.addListeners();
+      this.validateConfig();
 };
+
 SimpleTableDirective.prototype.validateConfig = function() {
-  //this.validateFilter();
+    //this.validateFilter();
 };
+
 SimpleTableDirective.prototype.validateFilter = function() {
-  // Currently not required
-  this.$log.log("validateFilter...", this.scope.tableConfig);
-  this.scope.$watch("tableConfig.filter", angular.bind(this, this.onFilterUpdated) );
+      // Currently not required
+      this.$log.log('validateFilter...', this.scope.tableConfig);
+      this.scope.$watch('tableConfig.filter', angular.bind(this, this.onFilterUpdated) );
 };
+
 SimpleTableDirective.prototype.onFilterUpdated = function(newValue, oldValue) {
-  // Currently not required
-  if((!newValue && !oldValue) || newValue === oldValue){
-    return;
-  }
-  this.$log.log("onFilterUpdated:", this.scope.tableConfig.filter);
+      // Currently not required
+      if((!newValue && !oldValue) || newValue === oldValue){
+        return;
+      }
+      this.$log.log('onFilterUpdated:', this.scope.tableConfig.filter);
 };
+
 SimpleTableDirective.prototype.addListeners = function(){
-  //this.scope.$on("$destroy", this.removeListeners());
+    //this.scope.$on("$destroy", this.removeListeners());
 };
+
 SimpleTableDirective.prototype.removeListeners = function(){
-  this.$log.log("removing listeners...", this);
+    this.$log.log('removing listeners...', this);
 };
 
 SimpleTableDirective.prototype.registerPlugin = function(plugin){
-  this.$log.log("initializing plugins...");
-  this.plugins.push(plugin);
-  this.$timeout(angular.bind(this, this.initPlugins), 0);
+      this.$log.log('initializing plugins...');
+      this.plugins.push(plugin);
+      this.$timeout(angular.bind(this, this.initPlugins), 0);
 };
 
 SimpleTableDirective.prototype.initPlugins = function(){
-  angular.forEach(this.plugins, function(plugin){
-    if(plugin.isInitialized() ){ return; }
-    plugin.onRegistered();
-  });
+    angular.forEach(this.plugins, function(plugin){
+        if(plugin.isInitialized() ){ return; }
+        plugin.onRegistered();
+    });
 };
 
 
-angular.module('simpletable.table', [])
-  .directive('stTable', ['$log', '$timeout', function($log, $timeout) {
-
+angular.module('simpletable.table', []).directive('stTable', ['$log', '$timeout', function($log, $timeout){
     return {
-      restrict: 'AE',
-      scope: {
-        tableConfig: "=",
-        tableData: "="
-      },
-      controller: function($scope, $element, $attrs) {
-        return new SimpleTableDirective($scope, $element, $attrs, $log, $timeout);
-      },
-      template:
-      "<table ng-class='tableConfig.classes'>" +
-      "<thead>" +
-      " <tr>" +
-      "   <th ng-repeat='hcol in tableConfig.columns' ng-style='hcol.style' ng-class='hcol.headerClass'>{{hcol.title}}</th>" +
-      " </tr>" +
-      "</thead>" +
-      "<tbody>" +
-      " <tr ng-repeat='row in tableData | filter:tableConfig.filter'>" +
-      "   <td ng-repeat='col in tableConfig.columns' ng-class='col.cellClass'>" +
-      "     <span ng-if='!col.template'>{{row[col.field]}}</span>     " +
-      "     <span ng-if='!!col.template' ng-include='col.template'></span>     " +
-      "   </td>" +
-      " </tr>" +
-      "</tbody>" +
-      "</table>"
+          restrict: 'AE',
+          scope: {
+                tableConfig: '=',
+                tableData: '='
+          },
+          controller: function($scope, $element, $attrs) {
+                return new SimpleTableDirective($scope, $element, $attrs, $log, $timeout);
+          },
+          template:
+              "<table ng-class='tableConfig.classes'>" +
+              "  <thead>" +
+              "    <tr>" +
+              "      <th ng-repeat='hcol in tableConfig.columns' ng-style='hcol.style' ng-class='hcol.headerClass' height='{{tableConfig.headerHeight}}'>" +
+              "        <div style='height: 100%; width: 100%;'>" +
+              "          <div style='float: left; width: 98%'>" +
+              "            {{hcol.title}}" +
+              "          </div>" +
+              "          <div st-table-resizable-handler style='background-color: #000011; height: 100%; float: left; cursor: e-resize; width: 2px;'></div>" +
+              "        </div>" +
+              "      </th>" +
+              "    </tr>" +
+              "  </thead>" +
+              "  <tbody>" +
+              "    <tr ng-repeat='row in tableData | filter:tableConfig.filter'>" +
+              "      <td ng-repeat='col in tableConfig.columns' ng-class='col.cellClass'>" +
+              "        <span ng-if='!col.template'>{{row[col.field]}}</span>     " +
+              "        <span ng-if='!!col.template' ng-include='col.template'></span>     " +
+              "      </td>" +
+              "    </tr>" +
+              "  </tbody>" +
+              "</table>"
     };
-
-
 }]);
--- app/js/view2/view2.js
+++ app/js/view2/view2.js
@@ -13,41 +13,42 @@ angular.module('myApp.view2', ['ui.router', 'simpletable'])
 .controller('View2Ctrl', ['$scope', "$log", function($scope, $log) {
 
     $scope.data = [
-      {id: 1, name: "Roy", lastname: "Bonilla", age: 25},
-      {id: 2, name: "Kib", lastname: "Vindas", age: 25},
-      {id: 3, name: "Roy", lastname: "Bonilla", age: 25},
-      {id: 4, name: "Kib", lastname: "Vindas", age: 25},
-      {id: 5, name: "Roy", lastname: "Bonilla", age: 25},
-      {id: 6, name: "Kib", lastname: "Vindas", age: 25},
-      {id: 7, name: "Roy", lastname: "Bonilla", age: 25},
-      {id: 8, name: "Kib", lastname: "Vindas", age: 25},
-      {id: 9, name: "Roy", lastname: "Bonilla", age: 25},
-      {id: 10, name: "Kib", lastname: "Vindas", age: 25},
-      {id: 11, name: "Roy", lastname: "Bonilla", age: 25},
-      {id: 12, name: "Kib", lastname: "Vindas", age: 25},
-      {id: 13, name: "Roy", lastname: "Bonilla", age: 25},
-      {id: 14, name: "Kib", lastname: "Vindas", age: 25}
+        {id: 1, name: "Roy", lastname: "Bonilla", age: 25},
+        {id: 2, name: "Kib", lastname: "Vindas", age: 25},
+        {id: 3, name: "Roy", lastname: "Bonilla", age: 25},
+        {id: 4, name: "Kib", lastname: "Vindas", age: 25},
+        {id: 5, name: "Roy", lastname: "Bonilla", age: 25},
+        {id: 6, name: "Kib", lastname: "Vindas", age: 25},
+        {id: 7, name: "Roy", lastname: "Bonilla", age: 25},
+        {id: 8, name: "Kib", lastname: "Vindas", age: 25},
+        {id: 9, name: "Roy", lastname: "Bonilla", age: 25},
+        {id: 10, name: "Kib", lastname: "Vindas", age: 25},
+        {id: 11, name: "Roy", lastname: "Bonilla", age: 25},
+        {id: 12, name: "Kib", lastname: "Vindas", age: 25},
+        {id: 13, name: "Roy", lastname: "Bonilla", age: 25},
+        {id: 14, name: "Kib", lastname: "Vindas", age: 25}
     ];
 
     $scope.columns = [
-      {title: "Id", field: 'id', style: {width: "25%"}, headerClass:["myclass1", "myclass2"], cellClass:["cellclass1", "cellclass2"], template: "js/view2/col1tpl.html"},
-      {title: "Name", field: 'name', style: {width: "50px"} },
-      {title: "Last Name", field: 'lastname', style: {width: "25%"} },
-      {title: "Age", field: 'age', style: {width: "25%"}}
+        {title: "Id", field: 'id', style: {width: "25%"}, headerClass:["myclass1", "myclass2"], cellClass:["cellclass1", "cellclass2"], template: "js/view2/col1tpl.html"},
+        {title: "Name", field: 'name', style: {width: "50px"} },
+        {title: "Last Name", field: 'lastname', style: {width: "25%"} },
+        {title: "Age", field: 'age', style: {width: "25%"}}
     ];
 
     $scope.tableConfig = {
-      classes: ["table", "table-bordered"],
-      columns: $scope.columns
+        classes: ["table", "table-bordered", 'table-padding'],
+        columns: $scope.columns,
+        headerHeight: '30px'
     };
 
     $scope.changeCols = function(){
-      var elem = $scope.columns.shift();
-      $scope.columns.push(elem);
+        var elem = $scope.columns.shift();
+        $scope.columns.push(elem);
     };
 
     $scope.validateFilter = function(){
-      $log.log($scope.tableConfig);
+        $log.log($scope.tableConfig);
     };
 
 
